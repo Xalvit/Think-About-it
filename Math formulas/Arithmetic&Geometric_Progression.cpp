77b6845d4299ad9765b606071f122dbcbfb8d85e
Xalvi <valov_2008@bk.ru>
2024-02-19 20:19:24 +0500

    new function in progressions

--- Math formulas/Arithmetic&Geometric_Progression.cpp
+++ Math formulas/Arithmetic&Geometric_Progression.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <math.h>
 
-float Arith_Progress(const float n, const float d, const float a1)
+double Difference_of_arithmetic_progression(const double a2, const double a1)
 {
-	std::cout << "\n" << a1 + d * (n - 1) << "\n";
+	// a2 = second arithmethic progression number
+	// a1 = first arithmethic progression number
+	std::cout << "\nArithmetic progression difference:\t" << a2 - a1 << std::endl;
+	return a2 - a1;
+}
+
+double Arith_Progress(const double n, const double d, const double a1)
+{
+	std::cout << "\nArith progress number:\t" << a1 + d * (n - 1) << "\n";
 	return a1 + d * (n - 1);
 }
-float Arith_Progress_Sum(const float n, const  float a1, const  float d)
+double Arith_Progress_Sum(const double n, const  double a1, const  double d)
 {
-	std::cout << "\n" << (2 * a1 + d * (n - 1)) * n / 2 << "\n";
+	std::cout << "\nArithmetic progress sum:\t" << (2 * a1 + d * (n - 1)) * n / 2 << "\n";
 	return ((2 * a1 + d * (n - 1)) / 2) * n;
 }
-float Geo_Progress(const float b1, const  float q, const float n)
+double Geo_Progress(const double b1, const  double q, const double n)
 {
-	std::cout << "\n" << b1 * pow(q, n - 1) << "\n";
+	std::cout << "\nGeometric progress:\t" << b1 * pow(q, n - 1) << "\n";
 	return b1 * pow(q, n - 1);
 }
-float Geo_Progress_Sum(const float b1, const float n, const float q) // q != 1
+double Geo_Progress_Sum(const double b1, const double n, const double q) // q != 1
 {
 	if (q != 1)
 	{
-		std::cout << (b1 * (pow(q, n) - 1)) / (q - 1);
+		std::cout << "Geometric progression sum:\t" << (b1 * (pow(q, n) - 1)) / (q - 1) << std::endl;
 		return (b1 * (pow(q, n) - 1)) / (q - 1);
 	}
 	else
@@ -28,6 +36,6 @@ float Geo_Progress_Sum(const float b1, const float n, const float q) // q != 1
 }
 int main()
 {
-
+	
 	return 0;
 }
